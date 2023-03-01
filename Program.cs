b95563c25d228ad2fe95b419268ff285d5d37ab7
Ilker Aydin <k.i.aydin0610@gmail.com>
2023-03-01 20:18:48 +0300

    ilk commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace hatakontrol
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            try
+            {
+            Console.WriteLine("Bir sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı: "+sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata :"+ ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem Tamamlandı");
+            }
+            
+
+            try
+            {
+                int a = int.Parse(null);
+            }
+            catch (ArgumentNullException ex) // null hatası, FormatException == format hatası, OverflowException == minimum veya maksimum değerlerin dışınd olması
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }
+            Console.ReadLine();
+        
+        }
+    }
+}
+
